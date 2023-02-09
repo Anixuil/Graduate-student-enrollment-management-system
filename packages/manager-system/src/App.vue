72fce4d858b37ffc1cb33ec97147969e2ba0aa0d
liuxin0207 <1140040227@qq.com>
2023-02-09 18:09:01 +0800

    feat: 添加页头动画和index部分页面内容

--- packages/manager-system/src/App.vue
+++ packages/manager-system/src/App.vue
@@ -8,7 +8,9 @@ const store = usePageHeader()
 
 <template>
     <HeaderNav></HeaderNav>
-    <PageHeader v-show="store.pageHeaderStatus"></PageHeader>
+    <Transition name="page-header">
+        <PageHeader v-show="store.pageHeaderStatus"></PageHeader>
+    </Transition>
     <RouterView v-slot="{ Component, route }">
         <KeepAlive>
             <Transition :name="(route.meta.transition as string)">
@@ -27,5 +29,32 @@ body {
 body::-webkit-scrollbar {
     width: 0;
     height: 0;
+}
+
+//页头过渡效果
+.page-header-enter-active,
+.page-header-leave-active {
+    transition: all 0.2s ease;
+}
+
+.page-header-enter-from,
+.page-header-leave-to {
+    opacity: 0;
+    transform: translateY(100%);
+}
+
+//页面跳转过渡效果
+.slide-fade-enter-active {
+    transition: all 0.4s ease;
+}
+
+.slide-fade-leave-active {
+    transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+    transform: translateY(20px);
+    opacity: 0;
 }
 </style>
--- packages/manager-system/src/views/index/index.vue
+++ packages/manager-system/src/views/index/index.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="index-box">hello world</div>
+    <div class="index-box">
+        <Banner></Banner>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Banner from '@/components/index/banner/index.vue'
+</script>
 
 <style scoped lang="scss">
 .index-box {
@@ -10,5 +14,9 @@
     min-width: 1000px;
     max-width: 1600px;
     border: 1px solid red;
+    background-color: white;
+    min-height: calc(100vh - (96px + 84px));
+    box-sizing: border-box;
+    padding: 0 20px;
 }
 </style>
